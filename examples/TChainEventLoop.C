1015098af00134b96f9ed03f9f59b0e7174db729
Jan Mayer <jan.mayer@ikp.uni-koeln.de>
2017-04-23 18:04:38 +0200

    Added example

---
+++ examples/TChainEventLoop.C
@@ -0,0 +1,21 @@
+void TChainEventLoop()
+{
+    SOCO::Event* event = new SOCO::Event();
+
+    TChain* chain = new TChain("ttree");
+    chain->Add("../build/out/*.root");
+    chain->SetBranchAddress("events", &event);
+    const Long64_t nEntries = chain->GetEntries();
+    std::cout << "Total number of events: " << nEntries << std::endl;
+
+    for (Long64_t i = 0; i < nEntries && i < 10; i++)
+    {
+        chain->GetEntry(i);
+        std::cout << i << " - " << event->timestamp << ": " << event->hits.size() << std::endl;
+        for (const auto& hit : event->hits)
+        {
+            std::cout << "\t" << hit.id << ":" << hit.adc;
+        }
+        std::cout << std::endl;
+    }
+}
